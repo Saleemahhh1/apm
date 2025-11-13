14ef34fb0106f2f30bc2246b9dcbef24dde44063
Saleemahhh1 <202877436+Saleemahhh1@users.noreply.github.com>
2025-11-13 12:27:20 +0100

    Add Tailwind CSS configuration with custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        apmGreen: '#1bb36b',
+        apmGold: '#d4af37',
+        bnbYellow: '#f3ba2f',
+        darkBg: '#0b1020'
+      }
+    }
+  },
+  plugins: []
+}
